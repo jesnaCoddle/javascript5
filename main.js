f0d15bf45d2f2826b3c83ee98b59d8e1f48dcbfb
jesnaCoddle <jesna.j@coddletech.com>
2025-02-21 12:24:51 +0530

    update'

--- main.js
+++ main.js
@@ -70,24 +70,3 @@ myButton.addEventListener("click", () => {
     console.log(toggleState);
 });
 
-//4
-
-var i=0;
-var images =[];
-var time=3000;
-
-images[0]='images\aston-martin.jpg';
-images[1]='images\maserati-mc20.jpg';
-images[2]='images\nio-et5.jpg';
-
-function changeImg(){
-    document.hasChildNodes.src=images[i];
-    if(i<images.length-1){
-        i++;
-    }
-    else{
-        i=0;
-    }
-    setTimeout("changeImg()",time)
-}
-window.onload=changeImg;
